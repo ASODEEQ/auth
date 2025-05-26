8b56013e2a2e773f1ddc4292b912aab87d721c7c
ASODEEQ <atolagbeolalekan1@gmail.com>
2025-05-26 23:14:18 +0100

    made changes by adding a logout function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  let navigate = useNavigate()
+  const logout=()=>{
+    
+    navigate('/login')
+
+  }
   return (
     <div>
                 
         <h1>yeaaa</h1>
+
+        <button onClick={logout}>Logout</button>
     </div>
   )
 }
